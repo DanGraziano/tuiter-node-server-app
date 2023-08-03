064d44f3601ec7917f52bae886c307b299a6d7d2
DanGraziano <dangraziano@outlook.com>
2023-08-03 12:40:04 -0400

    Add dislike stats to tuits

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -12,6 +12,8 @@ export default [
      "replies": 123, 
      "retuits": 432, 
      "tuit": "You want to wake up in the morning and think the future is going to be great - and that's what being a spacefaring civilization is all about. It's about believing in the future and thinking that the future will be better than the past. And I can't think of anything more exciting than going out there and being among the stars",
+     "disliked": false,
+     "dislikes": 345
     },
     {
      "_id": "234", 
@@ -22,8 +24,11 @@ export default [
      "image": "/images/boring-logo.png", 
      "liked": true, 
      "likes": 2345, 
-     "replies": 321, "retuits": 432, 
-     "title": "The Boring Company fixing traffic", "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems."
+     "replies": 321, 
+     "retuits": 432, 
+     "title": "The Boring Company fixing traffic", "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+     "disliked": true,
+     "dislikes": 123
     },
     {
      "_id": "345", 
@@ -33,7 +38,13 @@ export default [
      "time": "3h", 
      "image": "/images/neurolink-logo.png", 
      "title": "Neurolink fixes paralisis and blindness", 
-     "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.", "liked": true, "likes": 3456, "replies": 345, "retuits": 654
+     "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.", 
+     "liked": true, 
+     "likes": 3456, 
+     "replies": 345, 
+     "retuits": 654,
+     "disliked": false,
+     "dislikes": 456
     },
-    ];
+];
     
