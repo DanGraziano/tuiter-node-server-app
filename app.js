901d0ae8a0a514f634a11083b7ae087951a0868f
DanGraziano <dangraziano@outlook.com>
2023-08-03 13:04:29 -0400

    Complete section 7

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import session from "express-session";
+import AuthController from "./users/auth-controller.js";
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(
+ cors({
+   credentials: true,
+   origin: "http://localhost:3000",
+ })
+);
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+
+app.use(session(sessionOptions));
+  
 app.use(express.json());
+const port = process.env.PORT || 4000;
+
 TuitsController(app);
-HelloController(app)
-UserController(app)
-app.listen(4000)
+HelloController(app);
+UserController(app);
+AuthController(app);
+
+app.listen(4000);
